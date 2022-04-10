abcffb4e51d2cbad6c1a717c04638e51da408ca7
Douglas Gonzaga <douglasgonzaga10@gmail.com>
2022-04-10 11:14:05 -0300

    Começando a colocar peças no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 Console.WriteLine("Hello World");
 
 
@@ -6,4 +7,9 @@ Console.WriteLine("Hello World");
 //p = new Posicao(3, 4);
 //Console.WriteLine("Posição:" + p);
 Tabuleiro tab = new (8, 8);
+
+tab.ColocarPeca(new Torre(tab, Cor.Branco), new Posicao(0, 0));
+tab.ColocarPeca(new Torre(tab, Cor.Branco), new Posicao(1, 3));
+tab.ColocarPeca(new Rei(tab, Cor.Branco), new Posicao(2, 4));
 Tela.ImprimirTabuleiro(tab);
+Console.WriteLine();
